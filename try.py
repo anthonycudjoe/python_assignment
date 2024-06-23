e8edf644eab84d391e4dbd65985ba0dd4e352106
anthonycudjoe <anthonycudjoe1738@gmail.com>
2024-06-23 16:22:05 +0000

    initial commit

---
+++ try.py
@@ -0,0 +1,9 @@
+# A code to check if a number is a 5 or 6
+number = int(input("input a number"))
+if number == 5:
+    print("number is 5")
+elif number == 6:
+        print("number is 6")
+else: 
+    print("number is not 5 or 6")
+
---
+++ try2.py
@@ -0,0 +1,7 @@
+# a code to multiply 2 numbers
+number_1 = int(input("Input a number \n"))
+number_2 = int(input("Input second number \n"))
+
+answer = number_1 * number_2
+print(answer)
+
