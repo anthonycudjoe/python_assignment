e88e604de6e4b6205dbcc490468e7b11d27b8cea
anthonycudjoe <anthonycudjoe1738@gmail.com>
2024-06-25 00:11:37 +0000

    initial commit

---
+++ try3.py
@@ -0,0 +1,29 @@
+#Grading system
+
+Mark = int(input("Enter your Mark: "))
+if Mark >=80:
+  print("A")
+elif Mark >= 75:
+    print("B+")
+elif Mark >= 70:
+    print("B")
+elif Mark >= 65:
+    print("C+")
+elif Mark >= 60:
+    print("C")
+elif Mark >= 55:
+    print("D+")
+elif Mark >= 50:
+    print("D")
+elif Mark >= 40:
+    print("E")
+else:
+    print("F")
+
+    print(Mark)
+    
+
+
+
+
+ 
